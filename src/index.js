5b5a258c995831ec7cd99d32d008d85cd74cf30c
Silveira Guimaraes <silveira_oi@yahoo.com.br>
2021-07-21 07:40:13 +0000

    Inicio

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: 0
   };
-
+  repositories.push(repository);
   return response.json(repository);
 });
 
@@ -30,7 +30,7 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const repositoryIndex = repositories.findindex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
@@ -46,7 +46,7 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex > 0) {
     return response.status(404).json({ error: "Repository not found" });
